a6ee4a1d1a53e671d4488d84bcb2e605ec0b4a97
prudhvinadh007 <prudhvinadh007@risksek.com>
2024-11-05 13:21:21 +0530

    new commit

--- myproject/manage.py
+++ myproject/manage.py
@@ -2,6 +2,7 @@ import os
 import django
 from django import forms
 from django.utils.encoding import uri_to_iri
+from django.utils.text import Truncator
 
 # Set up Django environment
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')  # Replace 'your_project.settings' with your settings module
@@ -20,6 +21,16 @@ def main():
     form = UploadMultipleFilesForm()
     print("Form HTML:")
     print(form.as_p())
+    
+    long_text = "This is an example of a long text that will be truncated to a specific number of characters."
+
+    # Create a Truncator instance
+    t = Truncator(long_text)
+
+    # Correctly call chars() with the desired character limit
+    truncated_html = t.chars(30, html=True)
+
+    print("Truncated text:", truncated_html)
 
 if __name__ == "__main__":
     main()
