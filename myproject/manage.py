a8128ae836090d65156613b807d3aa5b3621f526
Ubuntu <ubuntu@agent-test.2dm0hjpxbyeufjyeh0ponoe3td.bx.internal.cloudapp.net>
2024-11-05 10:27:05 +0000

    checking for another cve-id

--- myproject/manage.py
+++ myproject/manage.py
@@ -1,7 +1,7 @@
 import os
 import django
-# from django import forms
-from django.utils.encoding import uri_to_iri
+from django import forms
+# from django.utils.encoding import uri_to_iri
 # from django.utils.text import Truncator
 
 # Set up Django environment
@@ -10,17 +10,17 @@ django.setup()
 
 def main():
     # Part 1: URI to IRI conversion
-    uri = "https://example.com/path/%C3%A9cole"
-    iri = uri_to_iri(uri)
-    print("Converted IRI:", iri)  # Expected output: https://example.com/path/école
+    #  uri = "https://example.com/path/%C3%A9cole"
+    # iri = uri_to_iri(uri)
+    # print("Converted IRI:", iri)  # Expected output: https://example.com/path/école
 
-    # Part 2: Django form with multiple file uploads
-    # class UploadMultipleFilesForm(forms.Form):
-    #     files = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+     # Part 2: Django form with multiple file uploads
+     class UploadMultipleFilesForm(forms.Form):
+         files = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
 
-    # form = UploadMultipleFilesForm()
-    # print("Form HTML:")
-    # print(form.as_p())
+     form = UploadMultipleFilesForm()
+     print("Form HTML:")
+     print(form.as_p())
     
     # long_text = "This is an example of a long text that will be truncated to a specific number of characters."
 
